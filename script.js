8d3b4183ac5104830c8121145e0492f5eb3c2aba
Lucas Marcelo <lucas.sampaio4@gmail.com>
2026-07-29 14:52:16 -0300

    feat: dados iniciais dos links como array de objetos. E pequena correção de espaçamento

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ if  (localStorage.getItem('tema') === 'claro') {
         btn.textContent = temaAtual ? '☀️' : '🌙';
         localStorage.setItem('tema', temaAtual ? 'claro' : 'escuro');
     });
+
+
+const linksIniciais = [
+{
+    id: "1",
+    titulo: "GitHub",
+    url: "https://github.com/luscasmarcelo",
+    icone: "fab fa-github",
+},
+{
+    id: "2",
+    titulo: "LinkedIn",
+    url: "https://linkedin.com/in/luscasmarcelo",
+    icone: "fab fa-linkedin",
+},
+{
+    id: "3",
+    titulo: "DevLinks",
+    url: "#",
+    icone: "fas-fa-code",
+},
+];
