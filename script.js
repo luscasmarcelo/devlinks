0daf5bd13785f005df32befcf756cc83b13a12de
Lucas Marcelo <lucas.sampaio4@gmail.com>
2026-07-31 16:38:00 -0300

    feat: Métodos aplicados no array linksIniciais com resultado no console

--- script.js
+++ script.js
@@ -34,6 +34,25 @@ const linksIniciais = [
 },
 ];
 
+const titulos = linksIniciais.map(link => link.titulo);
+console.log("títulos extraidos: ", titulos);
+
+const comUrl = linksIniciais.filter(link => link.url !== "#");
+console.log("links com URL: ", comUrl);
+
+const github = linksIniciais.find(link => link.titulo === "GitHub");
+console.log("Link do github: ", github);
+
+linksIniciais.forEach(link => console.log(link.titulo));
+
+linksIniciais.push({
+    id: "4",
+    titulo: "Meu portifólio",
+    url: "#",
+    icone: "fas fa-laptop-code",
+});
+console.log("Array atualizado (agora tem quatro itens.): ", linksIniciais);
+
 const gerarId = () => Date.now().toString();
 const urlValida = (url) => {
     try {
